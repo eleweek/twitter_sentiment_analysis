7a0aa1c0ab0c68c3082860750b3216a0758d70cf
Alexander Putilin <avp-13@yandex.ru>
2017-03-24 03:11:22 +0300

    Add SimpleUnigramModel

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 import logging
 import random
+import re
+
+from collections import OrderedDict
 
 from gensim.models.doc2vec import TaggedDocument
 from gensim.models import Doc2Vec
@@ -15,6 +18,11 @@ class TweetToFeaturesModel(object):
 
 
 class Fasttext(TweetToFeaturesModel):
+    """
+    Represents class for Facebook's fasttext models.
+    Pre-trained models can be found here:
+    https://github.com/facebookresearch/fastText/blob/master/pretrained-vectors.md
+    """
     model_name = "fasttext"
 
     @staticmethod
@@ -90,3 +98,70 @@ class SimpleDoc2Vec(TweetToFeaturesModel):
     @staticmethod
     def _train_item_tag(i):
         return "TRAIN_ITEM_{}".format(i)
+
+
+class RussianSentimentLexicon(object):
+    """
+    Represents a class for sentiment lexicon created in:
+    [Chetviorkin I. I. , Loukachevitch N. V. Extraction of Russian Sentiment Lexicon for Product Meta-Domain
+     // In  Proceedings of COLING 2012: Technical Papers , pages 593–610]
+    """
+    def __init__(self, filename):
+        word_to_prob = {}
+        with open(filename) as f:
+            for line in f:
+                m = re.match('(\w+)\t([0-9.]+)')
+                if m:
+                    word = m.group(1)
+                    probability = float(m.group(2))
+                    word_to_prob[word] = probability
+
+        self._word_to_prob = OrderedDict()
+        for index, word in enumerate(sorted(word_to_prob.iterkeys())):
+            self._word_to_prob[word] = word_to_prob[word]
+            self._word_to_index[word] = index
+
+    def size(self):
+        return len(self._word_to_prob)
+
+    def word_pos(self, word):
+        return self._word_to_index.get(word)
+
+    def words(self):
+        return self._word_to_prob.keys()
+
+    def sentiment_probability(self, word):
+        return self._word_to_prob.get(word.upper(), 0.0)
+
+    def has_sentiment(self, word):
+        return self.sentiment_probability(self, word) > 0
+
+
+class SimpleUnigramModel(TweetToFeaturesModel):
+    model_name = "simple_unigram"
+
+    def __init__(self, sentiment_lexicon_filename):
+        self.lexicon = RussianSentimentLexicon(sentiment_lexicon_filename)
+
+    @staticmethod
+    def create_from_argv(*args):
+        assert len(args) == 1
+        return SimpleUnigramModel(*args)
+
+    @staticmethod
+    def load(filename):
+        return SimpleUnigramModel(filename)
+
+    def train(self, train_data):
+        assert NotImplementedError("SimpleUnigramModel doesn't need any training. Simply pass sentiment lexicon file to it")
+
+    def get_features_number(self):
+        return self.lexicon.size()
+
+    def get_features(self, tweet):
+        features = [0] * self.get_features_number()
+        for word in tweet.words:
+            idx = self.lexicon.word_pos(word)
+            features[idx] = 1
+
+        return features
